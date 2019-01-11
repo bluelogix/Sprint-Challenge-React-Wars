74b3e3c440c3ec1895753162d38fc0b507ceccdf
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-01-11 12:53:45 -0500

    Was able to get the names to render on the screen, added one element to each name

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,8 +3,15 @@ import React from 'react';
 const Character = props => {
   return (
     <div>
-    <p>{props.info.name}</p>
-    </div>
+    <p>{props.charList.name}</p>
+        <div className="elementList">
+           <div>gender: {props.charList.gender}</div>
+        </div>
+
+
+    </div> // end div
+
+
   )}
 
 export default Character;
